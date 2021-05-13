316f8ef4109b76ea9b2d1c04b678bad7f1a87b64
Mary Bovtramovich <m.abovt@mail.ru>
2021-05-13 18:20:50 +0300

    [add modal window]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -9,11 +8,13 @@ import {MatListModule} from '@angular/material/list';
 import {MatInputModule} from '@angular/material/input';
 import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
+import {MatDialogModule} from '@angular/material/dialog';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { HeaderComponent } from './header/header.component';
 import { ProfileSidebarComponent } from './profile-sidebar/profile-sidebar.component';
 import { WishCardComponent } from './wish-card/wish-card.component';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { SearchInputComponent } from './search-input/search-input.component';
+import { ModalWindowComponent } from './modal-window/modal-window.component';
 
 @NgModule({
   declarations: [
@@ -22,6 +23,7 @@ import { SearchInputComponent } from './search-input/search-input.component';
     ProfileSidebarComponent,
     WishCardComponent,
     SearchInputComponent,
+    ModalWindowComponent,
   ],
   imports: [
     BrowserModule,
@@ -33,6 +35,7 @@ import { SearchInputComponent } from './search-input/search-input.component';
     MatAutocompleteModule,
     MatCardModule,
     MatIconModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/wish-card/wish-card.component.ts
+++ src/app/wish-card/wish-card.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { IWish } from '../../interface';
+import { ModalWindowComponent } from '../modal-window/modal-window.component';
 
 @Component({
   selector: 'app-wish-card',
@@ -8,4 +10,14 @@ import { IWish } from '../../interface';
 })
 export class WishCardComponent {
   @Input() wish: IWish;
+
+  constructor(public dialog: MatDialog) {}
+
+  openDialog() {
+    this.dialog.open(ModalWindowComponent, {
+      width: '370px',
+      maxWidth: '80%',
+      height: '200px',
+    });
+  }
 }
