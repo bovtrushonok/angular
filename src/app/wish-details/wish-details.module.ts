9b8c0dd1739fbab8d66f9b6ee44b8f8e43e59064
Mary Bovtramovich <m.abovt@mail.ru>
2021-06-16 23:17:27 +0300

    feat wish-details module

--- src/app/wish-details/wish-details.module.ts
+++ src/app/wish-details/wish-details.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { WishDetailsComponent } from './wish-details/wish-details.component';
 
-
+const wishDetailsRoutes: Routes = [
+  {path: '', component: WishDetailsComponent}
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [WishDetailsComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterModule.forChild(wishDetailsRoutes)
   ]
 })
 export class WishDetailsModule { }
